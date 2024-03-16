b33fe6af4bdc7f1c3cba51b344e048e9fec9139a
nilay <nilayanand@gmail.com>
2024-03-16 16:01:40 +0530

    Contact form api

--- collector.php
+++ collector.php
@@ -1,3 +1,62 @@
 <?php
-phpinfo();
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST');
+header("Access-Control-Allow-Headers: X-Requested-With");
+
+require_once('env.php');
+
+$allowed_hosts = array('localhost', 'chandigarhcaterer.in');
+if (!isset($_SERVER['HTTP_HOST']) || !in_array($_SERVER['HTTP_HOST'], $allowed_hosts)) {
+    header($_SERVER['SERVER_PROTOCOL'].' 400 Bad Request');
+    exit;
+}
+
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+
+$first_name = mysqli_real_escape_string($conn, $_REQUEST['first_name']);
+$last_name = mysqli_real_escape_string($conn, $_REQUEST['last_name']);
+$email = mysqli_real_escape_string($conn, $_REQUEST['email']);
+$phone = mysqli_real_escape_string($conn, $_REQUEST['phone']);
+$ip = get_client_ip();
+$request_data = mysqli_real_escape_string($conn, json_encode($_REQUEST));
+
+
+$sql = "INSERT INTO leads (first_name, last_name, email, phone, ip, request_data, created_at, updated_at )
+VALUES ('$first_name', '$last_name', '$email', '$phone', '$ip', '$request_data', now(), now() )";
+
+
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+
+// Function to get the client IP address
+function get_client_ip() {
+  $ipaddress = '';
+  if (isset($_SERVER['HTTP_CLIENT_IP']))
+      $ipaddress = $_SERVER['HTTP_CLIENT_IP'];
+  else if(isset($_SERVER['HTTP_X_FORWARDED_FOR']))
+      $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
+  else if(isset($_SERVER['HTTP_X_FORWARDED']))
+      $ipaddress = $_SERVER['HTTP_X_FORWARDED'];
+  else if(isset($_SERVER['HTTP_FORWARDED_FOR']))
+      $ipaddress = $_SERVER['HTTP_FORWARDED_FOR'];
+  else if(isset($_SERVER['HTTP_FORWARDED']))
+      $ipaddress = $_SERVER['HTTP_FORWARDED'];
+  else if(isset($_SERVER['REMOTE_ADDR']))
+      $ipaddress = $_SERVER['REMOTE_ADDR'];
+  else
+      $ipaddress = 'UNKNOWN';
+  return $ipaddress;
+}
 ?>
